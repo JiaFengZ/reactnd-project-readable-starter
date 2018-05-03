aae2eaa815c986af08b21ab56a4b6c8c17fc7a47
jiafeng zhong <1040185058@qq.com>
2018-05-03 20:32:21 +0800

    Update NotFount.js

--- frontend/src/components/share/NotFount.js
+++ frontend/src/components/share/NotFount.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function NotFount() {
   	return (
-    	<div className="not-fount">
+    	  <div className="not-fount">
         	<span>对不起！你要访问的资源不存在了<img alt="not found" src={require('../../images/face.png')}/></span>
       	</div>
     )
